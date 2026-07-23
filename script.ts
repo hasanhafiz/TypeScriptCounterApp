f83a18811ce0b851a75bcbad718be28c823347e0
hasanhafiz <hasanhafizsakil@gmail.com>
2026-07-23 09:21:00 +0600

    add comment to displayCounter function

--- script.ts
+++ script.ts
@@ -7,13 +7,14 @@ let count = 0; // initialize counter value to 0;
 
 function displayCounter() {
     // Since, All three event listener usage this, we make a seperate function to avoid redundency and Follow the DRY principle.
+
+    // counterValue.textContent = count; // Type 'number' is not assignable to type 'string'. So, convert it to string
+    // counterValue.textContent = count.toString(); // All three event listener use this. This is redundent. So, apply Dry Principle.
     counterValue.textContent = count.toString();
 }
 
 incrementBtn.addEventListener('click', () => {
     count++;
-    // counterValue.textContent = count; // Type 'number' is not assignable to type 'string'. So, convert it to string
-    // counterValue.textContent = count.toString(); // All three event listener use this. This is redundent. So, apply Dry Principle.
     displayCounter();
 });
 
